3740fdcf00fd552612c16c7d735076fe88486440
abhioshar <abhioshar@gmail.com>
2023-02-20 11:55:52 +0530

    Refactor

--- src/config/Config.java
+++ src/config/Config.java
@@ -1,7 +1,7 @@
 package config;
 
 public interface Config {
-    String INPUT_FILE_PATH = "src/files/input.txt";
-    String OUTPUT_FILE_PATH = "src/files/output.txt";
+    String INPUT_FILE_PATH = "files/input.txt";
+    String OUTPUT_FILE_PATH = "files/output.txt";
     String OUTPUT_LINE_PREFIX = "The changing word was: ";
 }
